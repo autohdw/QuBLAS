393a3ee3a62fe98de89cc6bc78f5fd18f5b1ea2f
Niurx <754346443@qq.com>
2024-06-26 23:49:13 +0800

    test

--- main.cpp
+++ main.cpp
@@ -9,30 +9,71 @@ using namespace QuBLAS;
 
 int main()
 {
-    using type1 = Qu<intBits<12>, fracBits<8>>;
-    using type2 = Qu<intBits<12>, fracBits<8>>;
-    using type3 = Qu<intBits<12>, fracBits<8>>;
-    using type4 = Qu<intBits<7>, fracBits<9>>;
+    using type1 = Qu<intBits<3>, fracBits<6>>;
+    using type2 = Qu<intBits<8>, fracBits<2>>;
+    using type3 = Qu<intBits<7>, fracBits<4>>;
+    using type4 = Qu<intBits<6>, fracBits<5>>;
 
-    using A_t = Qu<dim<3, 3>, type1>;
-    using L_t = Qu<dim<3, 3>, type2>;
-    using D_t = Qu<dim<3>, type3>;
+    // create complex type
+    using c_t_1 = Qcomplex<type1, type2>;
+    using c_t_2 = Qu<type3, type4>; // identical to Qcomplex<type3, type4>
 
-    A_t A = {69, 27, 67, 27, 38, 72, 67, 72, 142};
-    L_t L;
-    D_t D;
+    c_t_1 a = {1, 2};
+    c_t_2 b = {3, 4};
 
-    Qsytrf<QsytrfSumLDArgs<type4>>(L, D, A);
+ 
+    type3 c = 113.123;
+    type4 d = 4;
 
-    L.display("L");
-    D.display("D");
+    using vec_t_1 = Qu<dim<6>, type1>;
+    using vec_t_2 = Qu<dim<3>, c_t_1>;
 
-    // using Linv_t = Qu<dim<3, 3>, type2>;
+    vec_t_1 v1 = {1, 2, 3, 4, 5, 6};
+    vec_t_2 v2 = {a, a, a};
 
-    // Linv_t Linv;
+    // complex mul
+    auto aa = Qmul<TFComplexMul<
+    abT<type2>,
+    cdT<type3>,
+    baT<type2>,
+    // abcT<type4>, // all tags are optional
+    // cdbT<type1>,
+    badT<type2>,
+    ABT<type2>,
+    BCT<type2>
+    >>(v2, v2);
 
-    // Qtrtri(Linv, L);
 
-    // Linv.display();
+
+
+
+
+
+
+    // vector to std::string
+    auto res = BitStream<r2l<3>,r2l<2>>(v1);
+
+    std::cout << res << std::endl;
+
+    v1.display();
+    // seperate res into 10 bits  each line
+    for (int i = 0; i < res.size(); i++)
+    {
+        if (i % 10 == 0)
+        {
+            std::cout << std::endl;
+        }
+        std::cout << res[i] << " ";
+
+    }
+
+    // std::string to vector, need to provide the target type
+    auto z = BitStream<vec_t_1,r2l<3>,r2l<2>>(res);
+
+    z.display();
+    
+ 
+    return 0;
+    
  
 }
