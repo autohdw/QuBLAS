63259ef1e6944fcce5c0abde5b81c58e494d7151
pikapika <dndxgzh@163.com>
2024-12-18 22:13:37 +0800

    add BitStream

--- main.cpp
+++ main.cpp
@@ -14,23 +14,20 @@ int main()
     auto z = BitStream<vecType, l2r, r2l<2>>(v_res);
     std::cout << z << "\n\n";
 
-    auto sum = v1 * v1;
-    std::cout << sum << '\n';
+    // ------------ complex vector type ------------
+    using type1 = Qu<isSigned<true>, intBits<3>, fracBits<2>, OfMode<SAT::ZERO>>;
+    using c_t = Qcomplex<type1, type1>;
+    using complex_vec_t = Qu<dim<3>, c_t>;
 
-    // // ------------ complex vector type ------------
-    // using type1 = Qu<isSigned<true>, intBits<3>, fracBits<2>, OfMode<SAT::ZERO>>;
-    // using c_t = Qcomplex<type1, type1>;
-    // using complex_vec_t = Qu<dim<3>, c_t>;
+    c_t complex1 = {1.5, 2};
+    c_t complex2 = {5, 0};
+    c_t complex3 = {3, 4};
 
-    // c_t complex1 = {1.5, 2};
-    // c_t complex2 = {5, 0};
-    // c_t complex3 = {3, 4};
-
-    // complex_vec_t complex_vec = {complex1, complex2, complex3};
-    // std::string c_res = BitStream<l2r, l2r>(complex_vec);
-    // std::cout << c_res << '\n';
-    // auto str2bit = BitStream<complex_vec_t, r2l<1>, l2r>(c_res);
-    // std::cout << str2bit << '\n';
+    complex_vec_t complex_vec = {complex1, complex2, complex3};
+    std::string c_res = BitStream<l2r, l2r>(complex_vec);
+    std::cout << c_res << '\n';
+    auto str2bit = BitStream<complex_vec_t, r2l<1>, l2r>(c_res);
+    std::cout << str2bit << '\n';
 
     return 0;
 }
